b803fe70dc4da687c20b9c4ef304588e73e18a3c
Vaishnavi1955 <phadvaishnavi1905@gmail.com>
2025-09-27 23:14:21 +0530

    Python ady one contains variables,operators,comments

---
+++ Day1.py
@@ -0,0 +1,71 @@
+print("Vaishnavi")
+print(type("Vaishnavi"))
+
+#Data Types string,integer,float,boolean
+#we can represent string in 3 ways
+name1="Krishna"
+name2='Krishna'
+name3='''Krishna'''
+print(name1,name2,name3)
+
+#float 
+pi=3.14
+print(pi)
+
+#bool
+is_raining=True
+print(is_raining)
+
+#type function
+print(type(name1))
+print(type(pi))
+print(type(is_raining))
+#type conversion
+print(int(pi))
+print(float(5))
+print(str(5))
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("Krishna"))
+print(bool([]))
+print(bool([1,2,3]))
+
+#print sum
+#arihmetic opeartors
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print(2**3)
+#relational operators
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+#logical operators
+print(a>5 and b>15)
+print(a>5 or b<15)
+print(not(a>5))
+
+#Assignment Operators
+c=5
+c+=5
+print(c)
+c-=2
+print(c)
+c*=2
+print(c)
+c/=2
+print(c)
+c//=2
+print(c)
+
+
